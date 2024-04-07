be41c8ef94169c6c94d266d8dc33a698b9456f38
TakeOne-Team <138727610+TakeOne-Team@users.noreply.github.com>
2024-04-07 12:36:34 +0100

    Delete Omega/plugin.video.jellyfin directory

--- Omega/plugin.video.jellyfin/jellyfin_kodi/entrypoint/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import division, absolute_import, print_function, unicode_literals
-
-#################################################################################################
-
-from ..helper import LazyLogger
-from ..jellyfin import Jellyfin
-
-#################################################################################################
